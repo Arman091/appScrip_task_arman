123943050062699556e692a1ec8a1d2cd4d97bad
Arman091 <rasoolarman7@gmail.com>
2024-03-22 18:15:47 +0530

    "filter_complete"

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -1,13 +1,14 @@
-
-import styles from './productItem.module.css';
-
+import styles from "./productItem.module.css";
+import Image from "next/image";
 const ProductItem = ({ product }) => {
   return (
     <div className={styles.product}>
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
-      <p>{product.description}</p>
-      <button>Add to Cart</button>
+      <div className={styles.heart}>
+        <p>{product.description}</p>
+        <Image src="/heart.svg" alt="error" width={10} height={10} />
+      </div>
     </div>
   );
 };
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -5,110 +5,13 @@ import ProductItem from "./ProductItem";
 import Filter from "../filter/Filter";
 import { useState, useEffect } from "react";
 import Pricefilter from "../filter/PriceFilter";
-const Products = () => {
+const Products = ({products}) => {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(""); // State to store selected category
   const toggleFilter = () => setIsFilterOpen(!isFilterOpen);
   const [isPriceFilteropen, setPriceFilter] = useState(false);
   const togglepriceFilter = () => setPriceFilter(!isPriceFilteropen);
-  const products = [
-    {
-      id: 1,
-      name: "T-shirt",
-      description: "Comfortable cotton t-shirt",
-      image: "/tshirt.jpg",
-      category: "men",
-      price: 15,
-    },
-    {
-      id: 2,
-      name: "Jeans",
-      description: "Classic denim jeans",
-      image: "/jeans.jpg",
-      category: "women",
-      price: 30,
-    },
-    {
-      id: 3,
-      name: "Sneakers",
-      description: "Stylish sneakers",
-      image: "/sneakers.jpg",
-      category: "men",
-      price: 50,
-    },
-    {
-      id: 4,
-      name: "Dress",
-      description: "Elegant black dress",
-      image: "/dress.jpg",
-      category: "women",
-      price: 45,
-    },
-    {
-      id: 5,
-      name: "Hoodie",
-      description: "Warm hoodie for chilly days",
-      image: "/hoodie.jpg",
-      category: "men",
-      price: 25,
-    },
-    {
-      id: 6,
-      name: "Skirt",
-      description: "Floral-printed skirt",
-      image: "/skirt.jpg",
-      category: "men",
-      price: 35,
-    },
-    {
-      id: 7,
-      name: "Jacket",
-      description: "Stylish jacket for all occasions",
-      image: "/jacket.jpg",
-      category: "men",
-      price: 55,
-    },
-    {
-      id: 8,
-      name: "Boots",
-      description: "Leather boots for rugged terrain",
-      image: "/boots.jpg",
-      category: "men",
-      price: 60,
-    },
-    {
-      id: 9,
-      name: "Sweater",
-      description: "Cozy knitted sweater",
-      image: "/sweater.jpg",
-      category: "women",
-      price: 40,
-    },
-    {
-      id: 10,
-      name: "Pants",
-      description: "Casual pants for everyday wear",
-      image: "/pants.jpg",
-      category: "baby",
-      price: 20,
-    },
-    {
-      id: 11,
-      name: "Blouse",
-      description: "Chic blouse for formal occasions",
-      image: "/blouse.jpg",
-      category: "baby",
-      price: 35,
-    },
-    {
-      id: 12,
-      name: "Shorts",
-      description: "Comfortable shorts for summer",
-      image: "/shorts.jpg",
-      category: "baby",
-      price: 18,
-    },
-  ];
+ 
 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
